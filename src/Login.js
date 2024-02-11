cff08bd3a2fda1f0b5ec4aa3c09eae06f5d7c955
michael-mendel <mmendel2501@gmail.com>
2024-02-11 15:23:26 -0500

    done year 2

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,7 @@ export default function Login() {
 
     return (
         <>
-            {!isVerified ? 
+            {isVerified ? 
                 <App /> :
                 (
                     <div className="loginBackground">
